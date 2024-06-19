a482af4ec10cb3be7f3a0463b7b0830f83f0ffac
Adnan Imran <imranadnan99@gmail.com>
2024-06-19 20:54:57 +0600

    Create A - Strings.cpp

---
+++ A - Strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+int main(){
+  
+  string a , b , ans; 
+  
+  cin >> a >> b ; 
+  
+  cout<< b+a << endl; 
+  
+  
+  
+  
+}
